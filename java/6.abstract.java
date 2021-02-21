bdd9fc53e222ba1b0bef2478dabb3d4da9b7eeb0
abhi <avp9795@gmail.com>
2021-02-21 15:41:01 -0500

    java lab qns

--- java/6.abstract.java
+++ java/6.abstract.java
@@ -1,18 +1,18 @@
-/*abstract class Shape{
+ abstract class Shape{
 abstract void numberOfSides();
 }
-*/
-class Rectangle {//extends Shape{
+class Rectangle extends Shape{
 void numberOfSides(){
 System.out.println("Number of sides of rectangle is 4");
 }}
 
-class Triangle {//extends Shape{
+class Triangle extends Shape{
 void numberOfSides(){
 System.out.println("Number of sides of Triangle is 3");
-}}
+}
+}
 
-class Hexagon {//extends Shape{
+class Hexagon extends Shape{
 void numberOfSides(){
 System.out.println("Number of sides of Hexagon is 6");
 }}
@@ -20,13 +20,13 @@ System.out.println("Number of sides of Hexagon is 6");
 class main{
 public static void main(String[] args){
 
-Hexagon h = new Hexagon();
+Shape h = new Hexagon();
 h.numberOfSides();
 
-Triangle t = new Triangle();
+Shape t = new Triangle();
 t.numberOfSides();
 
-Rectangle r = new Rectangle();
+Shape r = new Rectangle();
 r.numberOfSides();
 }
 }
